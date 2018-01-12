2d8cb4496402abf2718e1fe76633d6f1e7488ba3
raghupa88 <raghupa88@users.noreply.github.com>
2018-01-12 11:54:39 +0000

    Delete index.ts

--- src/index.ts
+++
@@ -1,1 +0,0 @@
-import { cube } from './content';import './assets/style.css';function component() {    let element = document.createElement('div');    element.innerHTML = [        'Hello webpack!',        '5 cubed is equal to ' + cube(5)    ].join('\n\n');    return element;}document.body.appendChild(component());
