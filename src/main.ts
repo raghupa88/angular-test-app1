3886c112c0bb03b008adcb74087f231bd064f07c
raghupa88 <raghupa88@users.noreply.github.com>
2018-01-12 11:54:01 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import { enableProdMode } from "@angular/core";
+//import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 //import { environment } from "./environments/environment";
-//if (environment.production) {//  enableProdMode();//}
-
-platformBrowserDynamic().bootstrapModule(AppModule)  .catch(err => console.log(err));
+//if (environment.production) {
+//  enableProdMode();
+//}
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(success => console.log("Bootstrap success"))
+  .catch(err => console.log(err));
